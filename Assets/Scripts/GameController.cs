f1b6b67e13c3e27d60f4beb97fabedbd56480618
Alexander Batuev <batuev.prizrak@gmail.com>
2021-01-07 13:23:18 +0900

    Перенес логику начала игры в отдельный скрипт

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject textBackground;
+
+    private bool _spaceAllowed;
+    
+    private void Start()
+    {
+        textBackground.transform.localScale = new Vector3(0.2f, 0.1f, 0.1f);
+        
+        Invoke(nameof(AllowSpace), 1f);
+    }
+
+    private void Update()
+    {
+        if (_spaceAllowed && Input.GetKeyDown(KeyCode.Space))
+        {
+            textBackground.transform.localScale = new Vector3(0.1f, 0.1f, 0.05f);
+
+            GetComponent<PlayerMove>().enabled = true;
+            GetComponent<PlayerFire>().enabled = true;
+            GetComponent<PlayerHealth>().enabled = true;
+            GetComponent<EnemySpawner>().enabled = true;
+            GetComponent<GameController>().enabled = false;
+        }
+    }
+
+    private void AllowSpace ()
+    {
+        _spaceAllowed = true;
+    }
+}
--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -3,32 +3,14 @@
 public class PlayerFire : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public GameObject textBackground;
     public Transform sourceTransform;
     public float bulletSpeed = 5f;
     public float bulletLifeTime = 5f;
-
-    private bool _notStarted = true;
-
-    private void Start()
-    {
-        textBackground.transform.localScale = new Vector3(0.2f, 0.1f, 0.1f);
-    }
-
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (_notStarted)
-            {
-                GetComponent<PlayerMove>().enabled = true;
-                GetComponent<PlayerHealth>().enabled = true;
-                GetComponent<EnemySpawner>().enabled = true;
-                _notStarted = false;
-                textBackground.transform.localScale = new Vector3(0.1f, 0.1f, 0.05f);
-                return;
-            }
-            
             var bullet = Instantiate(bulletPrefab);
             bullet.tag = "PlayerBullet";
             
